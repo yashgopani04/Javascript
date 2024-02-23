edee94be09d7704a6ed2cce8ebce743e0b16fdde
yashgopani04 <142729771+yashgopani04@users.noreply.github.com>
2024-02-23 18:15:41 +0100

    Strings methods

---
+++ Basics/string.js
@@ -0,0 +1,23 @@
+const firstName = "Yash"
+
+const lastName = "Gopani"
+
+// New and up to date method to combine two string
+
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
+
+
+const fullName = "YashGopani"
+
+console.log(fullName.length);
+console.log(fullName.charAt(4));
+console.log(fullName.indexOf('o'));
+
+
+const newString = fullName.substring(2 , 7)
+
+console.log(newString);
+
+const newString1 = fullName.slice(5 , 9)
+
+console.log(newString1);
