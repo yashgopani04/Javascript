c38d97a830e5648fcb57a8a60a86d18cf5972bf6
yashgopani04 <142729771+yashgopani04@users.noreply.github.com>
2024-02-25 16:28:23 +0100

    Arrays_1

---
+++ Basics_2/arrays.js
@@ -0,0 +1,38 @@
+let myarray = [45, 69, 85, "Yash", "Gopani"];
+
+
+// myarray.push(98)
+// myarray.pop()
+
+// console.log(myarray.includes("Yash"));
+// console.log(myarray.indexOf("Yash"));
+
+let mynewArry = [48, 85, 25, 96]
+
+// join() use to convert array into a string
+
+mynewArry1 = myarray.join()
+
+// console.log(mynewArry);
+// console.log(mynewArry1);
+
+// console.log(typeof mynewArry1);
+
+
+// let num1 = 400;
+
+// console.log(num1);
+
+let scoreA = [45, 98, 65, 55]
+
+console.log("A ", scoreA);
+
+let newScore = scoreA.slice(1,3)
+
+console.log("B ", newScore);
+
+let newScore1 = scoreA.splice(1,3)
+
+console.log("C ", newScore1);
+
+console.log("D ", scoreA);
