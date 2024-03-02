05947df66619d548863f0219485279da125077ef
yashgopani04 <142729771+yashgopani04@users.noreply.github.com>
2024-03-02 12:06:31 +0100

    Object_destructuring

---
+++ Basics_2/onjects_destructuring.js
@@ -0,0 +1,17 @@
+let myData = {
+
+    myFName : "Yash",
+    myLName : "Gopani",
+    myEmail : "yashgopani04@gmail.com",
+    myNumber : 9773448807
+    
+}
+
+// How to destructuring of Objects
+
+const {myEmail : Yash_Email} = myData
+
+// You can also assign a differnet name by your choice
+
+console.log(Yash_Email);
+
