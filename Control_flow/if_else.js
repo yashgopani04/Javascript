a129ea83744bae159a5cd7c7e48b0ae3a0fd61f4
yashgopani04 <142729771+yashgopani04@users.noreply.github.com>
2024-03-03 22:59:12 +0100

    if_else in js

---
+++ Control_flow/if_else.js
@@ -0,0 +1,26 @@
+const number = 1000
+const id = 4582
+
+let username = "yashgopani04"
+
+if (number <= 500) {
+    
+    console.log(`Welcome back ${username}`);
+
+}
+else if (number > 750 && id > 6000){
+
+    console.log(`Please enter valid username`);
+
+}
+
+else if(number > 1500 || id > 6000){
+
+    console.log(`Welcome back user${username} and your id is ${id}`);
+
+}
+else
+{
+
+    console.log(`chack your log in details`);
+}
